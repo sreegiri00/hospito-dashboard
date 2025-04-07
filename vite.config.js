a9444206a413f91ab7553383a9884c4ae0b1ddd4
Sree Giri <sreegiri00@gmail.com>
2025-04-07 14:38:24 +0530

    vite config base /hospito-dashboard/

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // ← this is important!
+  base: '/hospito-dashboard/',  // ← this is important!
   plugins: [react()],
 })
